bb104b4dd595ba7dca6242eaf7a39b8ef5f3f180
김현수 <tomy9729@naver.com>
2022-06-30 01:25:50 +0900

    [Backend] FetchRiotApi 클래스화 및 정리

--- BackEnd/loltochess/FetchRiotApi.js
+++ BackEnd/loltochess/FetchRiotApi.js
@@ -1,14 +1,16 @@
 import { apiKey } from "./apiKey.js"
-import fetch from "node-fetch"
 import axios from 'axios'
-
+import AxiosUtil from "./AxiosUtil.js"
 const url = "https://kr.api.riotgames.com/tft/summoner/v1/summoners/by-name/"
-const userName = "moz1e"
 const key = `?api_key=${apiKey}`
 
-fetch(`${url}${userName}${key}`)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+class FetchRitoApi {
+    getUserInfoByUserName(userName) {
+        const encodeUserName = encodeURI(userName)
+        return AxiosUtil.handle(axios.get(`${url}${encodeUserName}${key}`))
+    }
+}
 
-axios.get(`${url}${userName}${key}`, { headers: { charset: "utf-8" } })
-    .then((res) => console.log(res.data))
+
+Object.freeze(FetchRitoApi)
+export default new FetchRitoApi()
