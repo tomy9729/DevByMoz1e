e329077d5ce39051c7d66e2d25e8f5f70cb570d2
김현수 <tomy9729@naver.com>
2022-11-02 00:39:43 +0900

    [BackEnd] express 미들웨어 예제

--- BackEnd/ExpressBasic/api.js
+++ BackEnd/ExpressBasic/api.js
@@ -1,6 +1,20 @@
 const express = require("express")
 const app = express()
 
+function logger(req, res, next) {
+    console.log("logger...")
+    next()
+}
+
+function logger2(req, res, next) {
+    console.log("logger2...")
+    next()
+}
+
+
+app.use(logger)
+app.use(logger2)
+
 const server = app.listen(8081, () => {
     console.log("Start Server : localhost:8081")
 })
