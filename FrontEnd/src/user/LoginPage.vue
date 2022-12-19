a3eb8f9dd5f78962254eac5f7c52402abb04c585
김현수 <tomy9729@naver.com>
2022-12-20 00:30:47 +0900

    [FrontEnd] LoginPage CSS

--- FrontEnd/src/user/LoginPage.vue
+++ FrontEnd/src/user/LoginPage.vue
@@ -1,23 +1,29 @@
 <template>
-    <DxForm :formData="userFormData" />
+    <div class="LoginPage-root">
+        <DxForm :formData="userFormData" />
+    </div>
 </template>
 
 <script>
 import DxForm from "devextreme-vue/form"
 import User from "./User"
 export default {
-    // TODO 이름 바꾸는거 고려 page
-    name: "LoginPage",
-    components: {
-        DxForm,
-    },
-    data() {
-        return {
-            userFormData: User.getDefault(),
-        }
-    },
+  // TODO 이름 바꾸는거 고려 page
+  name: "LoginPage",
+  components: {
+    DxForm,
+  },
+  data() {
+    return {
+      userFormData: User.getDefault(),
+    }
+  },
 }
 </script>
 
 <style>
+.LoginPage-root{
+    height: 100%;
+    width:100%
+}
 </style>
