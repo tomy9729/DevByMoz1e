00b5593cabfc717cf594eb83aa0a7e495c569b6e
김현수 <tomy9729@naver.com>
2022-12-20 00:31:29 +0900

    [FrontEnd] MyLayout css

--- FrontEnd/src/MyLayout.vue
+++ FrontEnd/src/MyLayout.vue
@@ -1,16 +1,30 @@
 <template>
-    <LoginPage />
+    <div class="myLayout-root" :style="`height:${windowHeight}px`">
+        <MainPage />
+    </div>
 </template>
 
 <script>
-import LoginPage from "@/user/LoginPage.vue"
+import MainPage from "@/MainPage.vue"
 export default {
-    name: "MyLayout",
-    components: {
-        LoginPage,
-    },
+  name: "MyLayout",
+  components: {
+    MainPage,
+  },
+   data() {
+    return {
+      windowHeight: 0,
+    }
+  },
+  mounted() {
+    this.windowHeight = window.innerHeight-16 //margin size 8*2
+  },
 }
 </script>
 
 <style>
+.myLayout-root{
+    width: 100%;
+    height:100%;
+}
 </style>
